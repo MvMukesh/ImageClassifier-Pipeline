d6a7426d8c0ee649dfffbd0963a95cd752a9ea35
MvMukesh <mukeshmanral777@gmail.com>
2023-12-23 11:42:50 +0530

    update setup file

--- setup.py
+++ setup.py
@@ -7,18 +7,16 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.1"
 
-REPO_NAME = "imageClassifier"
+REPO_NAME = "ImageClassifier-Pipeline"
 AUTHOR_USER_NAME = "MvMukesh"
 SRC_REPO = "imageClassifier"
 AUTHOR_EMAIL = "mukeshmanral777@gmail.com"
 
 
 setuptools.setup(
-    name=SRC_REPO,
-    version=__version__,
-    author=AUTHOR_USER_NAME,
+    name=SRC_REPO,version=__version__,author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="Python package using CNN for imageClassification",
+    description="Python package for imageClassification",
     long_description=long_description,
     long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
