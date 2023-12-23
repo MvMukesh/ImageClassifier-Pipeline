2040fe19c6df6ec2e4390a7a96e8e7f1a6be189c
MvMukesh <mukeshmanral777@gmail.com>
2023-12-23 18:32:52 +0530

    training and eval code added

--- main.py
+++ main.py
@@ -28,3 +28,26 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+#3
+# STAGE_NAME = "Training"
+# try: 
+#    logger.info(f"*******************")
+#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+#    model_trainer = ModelTrainingPipeline()
+#    model_trainer.main()
+#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
+
+# #4
+# STAGE_NAME = "Evaluation stage"
+# try:
+#    logger.info(f"*******************")
+#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+#    model_evalution = EvaluationPipeline()
+#    model_evalution.main()
+#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
