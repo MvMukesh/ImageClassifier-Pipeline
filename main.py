bf6a83c8a57b77dbed7830c69027598fc322d378
MvMukesh <mukeshmanral777@gmail.com>
2024-01-06 11:43:05 +0530

    code snipet to check base model training

--- main.py
+++ main.py
@@ -1,53 +1,53 @@
 from imageClassifier import logger
 from imageClassifier.pipeline.data_ingestion_v01 import DataIngestionTrainingPipeline
-#from imageClassifier.pipeline.modelPrep_base_v02 import PrepareBaseModelTrainingPipeline
+from imageClassifier.pipeline.modelPrep_base_v02 import PrepareBaseModelTrainingPipeline
 
 
 #1
-STAGE_NAME = "Data Ingestion Stage"
+STAGE_NAME = "Data Ingestion | Stage-1"
 try:
-    logger.info(f">>>>>> Stage:1  {STAGE_NAME} started <<<<<<")
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
     
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
     
-    logger.info(f">>>>>> Stage:1  {STAGE_NAME} completed <<<<<<\n\n<========>")
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\n<========>")
 except Exception as e:
     logger.exception(e)
     raise e
 
-# #2
-# STAGE_NAME = "Model Prep Base"
-# try: 
-#    logger.info(f"*******************")
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#    prepare_base_model = PrepareBaseModelTrainingPipeline()
-#    prepare_base_model.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+#2
+STAGE_NAME = "Base Model Prep | Stage-2"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 #3
-# STAGE_NAME = "Training"
+# STAGE_NAME = "Training Model | Stage-3"
 # try: 
 #    logger.info(f"*******************")
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+#    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
 #    model_trainer = ModelTrainingPipeline()
 #    model_trainer.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+#    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
 # except Exception as e:
 #         logger.exception(e)
 #         raise e
 
 # #4
-# STAGE_NAME = "Evaluation stage"
+# STAGE_NAME = "Evaluation | Stage-4"
 # try:
 #    logger.info(f"*******************")
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+#    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
 #    model_evalution = EvaluationPipeline()
 #    model_evalution.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+#    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
 # except Exception as e:
 #         logger.exception(e)
 #         raise e
--- src/imageClassifier/components/__init__.py
+++ src/imageClassifier/components/__init__.py
@@ -1,2 +1,2 @@
 from imageClassifier.components.data_ingestion import DataIngestion
-#from imageClassifier.components.modelPrep_base import PrepareBaseModel
+from imageClassifier.components.modelPrep_base import PrepareBaseModel
